8ceeaa78d2217c24869de49f018854048b91b1f0
Deepak Patil <108725514+DeepakkPatil@users.noreply.github.com>
2023-05-06 18:27:09 +0530

    ipod added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./assets/css/index.css";
-import App from "./components/stateful/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/index.css';
+import App from './components/App';
 
-// Rendering the React App into the HTML DOM Element "root"
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById("root")
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
